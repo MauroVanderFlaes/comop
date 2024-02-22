47576823d9ff4ba852f142b3d52e652f488a1a3a
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2024-02-22 12:59:48 +0100

    feat: adding logo

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/comop.png')} style={{ width: 220, height: 50 }} />
       <StatusBar style="auto" />
     </View>
   );
